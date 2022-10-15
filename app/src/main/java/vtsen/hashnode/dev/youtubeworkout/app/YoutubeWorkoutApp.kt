ff0fe4b20afd121af9ab5d882bd81c7e0b74077f
vinchamp77 <vinchamp77@gmail.com>
2022-10-15 19:28:43 +1100

    Add YoutubeWorkoutApp

---
+++ app/src/main/java/vtsen/hashnode/dev/youtubeworkout/app/YoutubeWorkoutApp.kt
@@ -0,0 +1,7 @@
+package vtsen.hashnode.dev.youtubeworkout.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YoutubeWorkoutApp : Application()
