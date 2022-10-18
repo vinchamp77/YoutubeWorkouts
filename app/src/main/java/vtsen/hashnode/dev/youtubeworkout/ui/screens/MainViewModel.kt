9853423bc9a3c0590b9b8ce82da26f7834838b56
vinchamp77 <vinchamp77@gmail.com>
2022-10-18 14:36:37 +1100

    Add ProgressiveMediaSource but doesn't work on youtube URL

--- app/src/main/java/vtsen/hashnode/dev/youtubeworkout/ui/screens/MainViewModel.kt
+++ app/src/main/java/vtsen/hashnode/dev/youtubeworkout/ui/screens/MainViewModel.kt
@@ -1,17 +1,30 @@
 package vtsen.hashnode.dev.youtubeworkout.ui.screens
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@UnstableApi
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val player: Player,
+    val player: Player,
 ): ViewModel() {
 
     init {
         player.prepare()
+
+        /*Note: does not work*/
+        val uri = Uri.parse("https://youtu.be/VRnUojlA4B8")
+        val mediaSource = ProgressiveMediaSource.Factory(DefaultHttpDataSource.Factory())
+            .createMediaSource(MediaItem.fromUri(uri))
+
+        player.addMediaItem(mediaSource.mediaItem)
     }
 
     override fun onCleared() {
