332cab91cd5c3faf69b75e8f9a8c5cc0f9ecfa1f
vinchamp77 <vinchamp77@gmail.com>
2022-10-28 12:18:10 +1100

    Upgrade libraries and add coil library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha02")
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.6.1")
 
     val composeVersion = "1.2.1"
     implementation("androidx.compose.ui:ui:$composeVersion")
@@ -71,4 +71,6 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
     // for hiltViewModel()
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    implementation("io.coil-kt:coil-compose:2.1.0")
 }
