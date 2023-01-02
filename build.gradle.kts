37bfbcb006ae1207721bc1d90f6b5fdd61ed9a90
vinchamp77 <vinchamp77@gmail.com>
2023-01-03 09:39:30 +1100

    Remove clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,3 @@ plugins {
     id("com.android.library") version "7.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
-
-tasks {
-    register("clean", Delete::class) {
-        delete(rootProject.buildDir)
-    }
-}
